35cf098cbe04d11c5a6f8d80dece4c167c86b824
Owen White <smouldering.dog@gmail.com>
2023-02-25 16:53:04 -0500

    your mom

--- plot.py
+++ plot.py
@@ -8,7 +8,8 @@ import parse_data # helper script
 # URL = input()
 # URL = 'https://raw.githubusercontent.com/owhite/ebike_data/main/datasets/first_set'
 # page = parse_data.get_web_page(URL)
-fname = 'datasets/first_set.json'
+fname = 'datasets/feb25_FW30_8000kw.json'
+title = 'FW=30, p_max =8000'
 page = parse_data.get_file_page(fname)
 data = parse_data.make_frame(page)
 
@@ -18,24 +19,37 @@ df['phaseA'] = np.sqrt( (df['idq_d'] * df['idq_d']) + (df['idq_q'] * df['idq_q']
 
 t = np.arange(len(df['ehz']))
 
-fig, ax1 = plt.subplots()
+fig, host = plt.subplots()
+fig.subplots_adjust(right=0.75)
+ax1 = host.twinx()
+ax2 = host.twinx()
+ax2.spines.right.set_position(("axes", 1.15))
 
-fig.suptitle('Date: dataset, etc', fontsize=16)
-ax1.set_xlabel("Samples", color='black')
-ax1.set_ylim([0, 500])
-
-
+fig.suptitle(title, fontsize=16)
 color = 'tab:red'
-ax1.set_ylabel("ehz", color=color)
-ax1.plot(t, df['ehz'], color=color)
-
-
-ax2 = ax1.twinx()  # axes shares the same x-axis
+host.set_ylim(0, 700)
+host.set_xlabel("Samples", color='black')
+host.tick_params(axis='y', labelcolor=color)
+host.set_ylabel("ehz", color=color)
+host.plot(t, df['ehz'], color=color, label = 'ehz')
+fig.legend(loc = "upper left")
 
 color = 'tab:blue'
-ax2.set_ylabel('phaseA', color=color)  
+ax1.set_ylabel('phaseA', color=color)  
+ax1.tick_params(axis='y', labelcolor=color)
+ax1.set_ylim(0, 40)
+ax1.plot(t, df['phaseA'], color=color, label = 'phase A')
+fig.legend(loc = "upper left")
+
+offset = 60
+color = 'tab:green'
+ax2.set_ylabel('Vbus', color=color)  
 ax2.tick_params(axis='y', labelcolor=color)
-ax2.plot(t, df['phaseA'], color=color)
+ax2.set_ylim(40, 90)
+ax2.plot(t, df['vbus'], color=color, label = 'Vbus')
+fig.legend(loc = "upper left")
+
+
 
 # plt.show()
 plt.draw()

1f578821914efaf12a2209b33e3290c2d21b48fe
Owen White <smouldering.dog@gmail.com>
2023-03-05 17:14:19 -0500

    new data sets

--- plot.py
+++ plot.py
@@ -1,5 +1,6 @@
 #! /usr/bin/env python3
 
+import sys, getopt
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
@@ -8,9 +9,18 @@ import parse_data # helper script
 # URL = input()
 # URL = 'https://raw.githubusercontent.com/owhite/ebike_data/main/datasets/first_set'
 # page = parse_data.get_web_page(URL)
-fname = 'datasets/mar2_F40_w_cap.json'
 
-print (fname)
+try:
+    opts, args = getopt.getopt(sys.argv[1:],"i:",["ifile="])
+except getopt.GetoptError:
+    print ('program.py -i <inputfile>')
+    sys.exit(2)
+
+fname = ""
+
+for opt, arg in opts:
+    if opt in ("-i", "--infile"):
+        fname = arg
 
 the_page = parse_data.get_json_file(fname)
 title = the_page['title']
@@ -32,7 +42,7 @@ ax3.spines.right.set_position(("axes", 1.2))
 
 fig.suptitle(title, fontsize=16)
 color = 'tab:red'
-host.set_ylim(0, 700)
+host.set_ylim(0, 550)
 host.set_xlabel("Samples", color='black')
 host.tick_params(axis='y', labelcolor=color)
 host.set_ylabel("ehz", color=color)
@@ -47,19 +57,19 @@ ax1.set_ylim(0, 140)
 ax1.plot(t, df[datatype], color=color, label = datatype)
 fig.legend(loc = "upper left")
 
-datatype = 'adc_req'
+datatype = 'FW_curr_req'
 color = 'tab:green'
 ax2.set_ylabel(datatype, color=color)  
 ax2.tick_params(axis='y', labelcolor=color)
-ax2.set_ylim(0, 1.1)
+ax2.set_ylim(-50, 50)
 ax2.plot(t, df[datatype], color=color, label = datatype)
 fig.legend(loc = "upper left")
 
-datatype = 'vbus'
+datatype = 'adc_ext1'
 color = 'black'
 ax3.set_ylabel(datatype, color=color)  
 ax3.tick_params(axis='y', labelcolor=color)
-ax3.set_ylim(50, 80)
+ax3.set_ylim(0, 4200)
 ax3.plot(t, df[datatype], color=color, label = datatype)
 fig.legend(loc = "upper left")
 
